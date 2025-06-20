71bb7ebebb40802d27757725987ada69ef4af99d
iflux <73718491+iflux@users.noreply.github.com>
2025-06-20 16:37:00 +0200

    Update install_flutter.sh

--- backend/install_flutter.sh
+++ backend/install_flutter.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-# Installe Flutter
+# Installe Flutter dans le dossier courant
 git clone https://github.com/flutter/flutter.git -b stable
 export PATH="$PATH:`pwd`/flutter/bin"
+
+# Vérifie que Flutter est installé correctement
 flutter doctor
+
+# Récupère les dépendances de Flutter
 flutter pub get
+
+# Compile l'application Flutter pour le Web
+flutter build web
